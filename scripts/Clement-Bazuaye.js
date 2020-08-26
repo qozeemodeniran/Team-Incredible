211bf4d19122f81d417dab5ec7a1c7180c56b76e
Odeniran Qozeem Banji <muhdqozeem@gmail.com>
2020-08-26 21:42:12 +0100

    a group task at hngternship

---
+++ scripts/Clement-Bazuaye.js
@@ -0,0 +1,8 @@
+function interShipDetails() {
+    let fullName = "Clement Femi Bazuaye";
+    let id = "HNG-02047";
+    let email = "bazuaye50@gmail.com";
+    let language = "JavaScript";
+    console.log("Hello World, this is "+ fullName +" with HNGi7 ID "+ id +" and email "+email+" using "+ language +" for Stage 2 task.")
+    }
+interShipDetails();
